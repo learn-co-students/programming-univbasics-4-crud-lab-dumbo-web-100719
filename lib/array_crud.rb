d54778a1c30f5c4bd4c394940edf901dfb8c0e97
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 01:27:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,31 +17,31 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  four_elements = [ "one", "two", "three", "four"]
+  four_elements = [ "one", "two", "three", "arrays!"]
   four_elements.pop
 end
 
 def remove_element_from_start_of_array(array)
-  four_elements = [ "one", "two", "three", "four"]
+  four_elements = [ "wow", "two", "three", "four"]
   four_elements.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  four_elements = [ "one", "two", "three", "four"]
+  four_elements = [ "am", "two", "three", "four"]
   four_elements[0]
 end
 
 def retrieve_first_element_from_array(array)
-  four_elements = [ "one", "two", "three", "four"]
+  four_elements = [ "wow", "two", "three", "four"]
   four_elements[0]
 end
 
 def retrieve_last_element_from_array(array)
-  four_elements = [ "one", "two", "three", "four"]
+  four_elements = [ "wow", "two", "three", "arrays!"]
   four_elements[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  four_elements = [ "one", "two", "three", "four"]
-  four_elements[1]="oonnee"
+  four_elements = [ "wow", "two", "three", "four"]
+  four_elements[1]="totally"
 end
